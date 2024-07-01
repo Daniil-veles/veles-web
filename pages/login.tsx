2a24d2a8b89838e12467a52ec4145c724f4a1bd4
Daniil-veles <d.suvorov@skveles.ru>
2024-07-01 17:49:09 +0300

    компонент

---
+++ pages/login.tsx
@@ -0,0 +1,13 @@
+// import './login.scss';
+// import "@/app/layout/"
+
+function Login(): JSX.Element {
+  return (
+    <div>
+         <h1>Login Page</h1>
+         <p>This is the about page content.</p>
+    </div>
+  );
+}
+
+export default Login;
