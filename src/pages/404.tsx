2596dd637ac6a89fa18fcb4b972984477f4c48c2
Daniil-veles <d.suvorov@skveles.ru>
2024-07-02 18:06:11 +0300

    create new page

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import './404.scss';
+
+function NotFound(): JSX.Element {
+  return (
+    <div>
+        Страница 404
+    </div>
+  );
+}
+
+export default NotFound;
--- src/pages/object/[id].tsx
+++ src/pages/object/[id].tsx
@@ -2,7 +2,8 @@ import { useRouter } from "next/router";
 
 function Object(): JSX.Element {
     const router = useRouter();
-    const { id, q, asPath, pathname } = router.query;
+    const { id, q } = router.query;
+    const { asPath, pathname } = router;
 
     console.log(asPath, pathname);
 
