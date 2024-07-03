f56ecc9d4515426c5cbb3cf6c798686b80e182ed
Daniil-veles <d.suvorov@skveles.ru>
2024-07-03 13:54:35 +0300

    connect tailwind, add root styles

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,15 @@
-import './404.scss';
+// import './404.scss';
+
+import Image from "next/image";
 
 function NotFound(): JSX.Element {
   return (
-    <div>
-        Страница 404
+    <div className="bg-sky-700">
+      {/* Страница 404 */}
+      <div className="d-flex justify-center ">
+        <Image priority src='http://localhost:3000/404.png' width={400} height={400} alt="Изображение" />
+        <img className='w-100' width={400} height={400} src="./404.png" alt="" />
+      </div>
     </div>
   );
 }
