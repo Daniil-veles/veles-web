3f8d7389c2c36d1ff34b6281e0d270e470d6ccc7
Daniil-veles <d.suvorov@skveles.ru>
2024-07-05 13:06:06 +0300

    search component

--- src/components/blocks/reviews/Reviews.tsx
+++ src/components/blocks/reviews/Reviews.tsx
@@ -1,56 +1,47 @@
 import styles from "./Reviews.module.scss";
 import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        initialSlide: 1,
-      },
-    },
-  ],
-};
+import Container from "@/components/container/Container";
+import { Carousel } from "@/MTailwind";
 
 const Reviews: React.FC = () => {
   return (
     <div className={styles.container}>
-      <Slider {...settings}>
-        <div>
-          <h3>Slide 1</h3>
+      <Container className={styles.customContainer}>
+        <div className="flex">
+          <div className={styles.content}>
+            <h2 className={styles.title}>Customers's quotes</h2>
+
+            <p className={styles.text}>
+              Brute laoreet efficiendi id his, ea illum nonumes luptatum pro.
+              Usu atqui laudem an.
+            </p>
+          </div>
+
+          <Carousel className="rounded-xl">
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+                alt="image 1"
+                className="h-full w-full object-cover"
+              />
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+                alt="image 2"
+                className="h-full w-full object-cover"
+              />
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+                alt="image 3"
+                className="h-full w-full object-cover"
+              />
+            </div>
+          </Carousel>
         </div>
-        <div>
-          <h3>Slide 2</h3>
-        </div>
-        <div>
-          <h3>Slide 3</h3>
-        </div>
-        <div>
-          <h3>Slide 4</h3>
-        </div>
-        <div>
-          <h3>Slide 5</h3>
-        </div>
-      </Slider>
+      </Container>
     </div>
   );
 };
