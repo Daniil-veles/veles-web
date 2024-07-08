0501a0892de3cb7b98632d5c468fa92f3bcf381f
Daniil-veles <d.suvorov@skveles.ru>
2024-07-08 08:29:57 +0300

    add default Carousel

--- src/components/blocks/reviews/Reviews.tsx
+++ src/components/blocks/reviews/Reviews.tsx
@@ -1,8 +1,9 @@
 import styles from "./Reviews.module.scss";
 import React from "react";
 import Container from "@/components/container/Container";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/card";
 // import { Carousel } from "@/components/ui/carousel";
-
 
 const Reviews: React.FC = () => {
   return (
@@ -18,15 +19,30 @@ const Reviews: React.FC = () => {
             </p>
           </div>
 
-          {/* <Carousel> */}
-            {/* <CarouselContent> */}
-              {/* <CarouselItem>...</CarouselItem>
-              <CarouselItem>...</CarouselItem>
-              <CarouselItem>...</CarouselItem> */}
-            {/* </CarouselContent> */}
-            {/* <CarouselPrevious /> */}
-            {/* <CarouselNext /> */}
-          {/* </Carousel> */}
+          <Carousel
+            opts={{
+              align: "start",
+            }}
+            className="w-full max-w-sm"
+          >
+            <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        <span className="text-3xl font-semibold">
+                          {index + 1}
+                        </span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
         </div>
       </Container>
     </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div
       className={cn(
-        "min-h-screen bg-background font-sans antialiased",
+        "min-h-screen font-sans antialiased",
         fontSans.variable
       )}
     >
