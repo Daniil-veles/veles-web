362af3973f3443f480b5ba7d5106ca1739d1468d
Daniil-veles <d.suvorov@skveles.ru>
2024-07-08 08:23:53 +0300

    add font Roboto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import type { AppProps } from 'next/app'
-import '../styles/global.scss'; 
+import type { AppProps } from "next/app";
+import "../styles/global.scss";
+import { Roboto as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  weight: ['300', '400', '500', '700'],
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div
+      className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable
+      )}
+    >
+      <Component {...pageProps} />
+    </div>
+  );
 }
